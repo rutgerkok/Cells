4d34bd27b50fc698cc571de7e0f1eede73d565e7
Rutger Kok <rutger.kok@live.com>
2019-10-05 07:46:15 +0200

    Let player already lose when there are no dividing cells anymore

--- Scenes/Cells.gd
+++ Scenes/Cells.gd
@@ -9,7 +9,6 @@ const NON_DIVIDING_CELL = preload("res://Scenes/NonDividingCell.tscn")
 
 var _previous_fraction_nondividing_cells = 0
 var _previous_max_live_time_nondividing_cells_seconds = 0
-var _previous_child_count = 0
 var _game_over = false
 
 # Called when the node enters the scene tree for the first time.
@@ -18,8 +17,7 @@ func _ready():
     
     _previous_fraction_nondividing_cells = Parameters.fraction_nondividing_cells
     _previous_max_live_time_nondividing_cells_seconds = Parameters.max_live_time_nondividing_cells_seconds
-    _previous_child_count = self.get_child_count()
-    
+
     for child in get_children():
         if self._has_signal(child, "divide"):
             child.connect("divide", self, "_on_cell_divide")
@@ -44,13 +42,14 @@ func _process(delta):
         _previous_max_live_time_nondividing_cells_seconds = Parameters.max_live_time_nondividing_cells_seconds
         emit_signal("game_modified")
         
-    # Check if there are still cells
-    var child_count = self.get_child_count()
-    if child_count == 0 and self._previous_child_count != 0:
-        if not self._game_over:
-            emit_signal("game_over")
-            self._game_over = true
-    self._previous_child_count = child_count
+    # Check if there are still divding cells
+    var found_dividing = false
+    for child in self.get_children():
+        if _has_signal(child, "divide"):
+            found_dividing = true
+    if not found_dividing and not self._game_over:
+        emit_signal("game_over")  # If not, then it's game over
+        self._game_over = true
     
     
 func _on_cell_out_of_bounds():
